ceba51ad6d5264ace51351819c959d6950e7746e
ATISHAY JAIN <58568534+atishay1743@users.noreply.github.com>
2021-01-09 04:12:07 +0530

    validaton

--- models/user.js
+++ models/user.js
@@ -2,12 +2,20 @@ const mongoose = require('mongoose');
 const UserSchema  = new mongoose.Schema({
   name :{
       type  : String,
-      required : true
+      required : true,
+        index: {
+            unique: true,
+        },
   } ,
   email :{
     type  : String,
-    required : true
-} ,
+    required : true,
+    index:{
+        unique:true,
+    },
+      match:/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+} 
+,
 password :{
     type  : String,
     required : true
